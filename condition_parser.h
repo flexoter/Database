db1c10d1aa870a7da663476ef491c27772418d0f
quickybrains <vadimbukovshin199813@gmail.com>
2020-06-28 17:31:11 +0300

    condition_parser: init commit

---
+++ condition_parser.h
@@ -0,0 +1,15 @@
+#ifndef H_CONDITION_PARSER
+#define H_CONDITION_PARSER
+
+#include "node.h"
+
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+shared_ptr<Node> ParseCondition(istream& is);
+
+void TestParseCondition();
+
+#endif /*H_CONDITION_PARSER*/
