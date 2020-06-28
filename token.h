9233693f7a8da7124310204b8b86c6c047aa84ff
quickybrains <vadimbukovshin199813@gmail.com>
2020-06-28 17:32:37 +0300

    token: init commit, tokeniser for condition_parser

---
+++ token.h
@@ -0,0 +1,26 @@
+#ifndef H_TOKEN
+#define H_TOKEN
+
+#include <sstream>
+#include <vector>
+using namespace std;
+
+enum class TokenType {
+  DATE,
+  EVENT,
+  COLUMN,
+  LOGICAL_OP,
+  COMPARE_OP,
+  PAREN_LEFT,
+  PAREN_RIGHT,
+};
+
+struct Token {
+  const string value;
+  const TokenType type;
+};
+
+vector<Token> Tokenize(istream& cl);
+
+
+#endif /*H_TOKEN*/
